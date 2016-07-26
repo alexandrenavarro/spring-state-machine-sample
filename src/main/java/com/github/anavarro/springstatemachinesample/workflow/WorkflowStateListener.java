4e93713850926e8bf7b29b3485a12d41b8e18af0
Alexandre Navarro <alexandre.j.navarro@gmail.com>
2016-07-26 21:02:48 +0200

    Update WorkflowStateListener.java

--- src/main/java/com/github/anavarro/springstatemachinesample/workflow/WorkflowStateListener.java
+++ src/main/java/com/github/anavarro/springstatemachinesample/workflow/WorkflowStateListener.java
@@ -8,70 +8,75 @@ import org.springframework.statemachine.listener.StateMachineListenerAdapter;
 import org.springframework.statemachine.state.State;
 import org.springframework.statemachine.transition.Transition;
 
+
+
 /**
- * Created by anavarro on 23/07/16.
+ * Created by anavarro on 26/07/16.
  */
 @Slf4j
-public class WorkflowStateListener
+public final class WorkflowStateListener
         extends StateMachineListenerAdapter<WorkflowState, WorkflowEvent> {
 
     @Override
-    public void stateChanged(State<WorkflowState, WorkflowEvent> from, State<WorkflowState, WorkflowEvent> to) {
+    public void stateChanged(final State<WorkflowState, WorkflowEvent> from, final State<WorkflowState, WorkflowEvent> to) {
         log.debug("stateChange from:{}, to:{}", from, to);
     }
 
     @Override
-    public void stateEntered(State<WorkflowState, WorkflowEvent> state) {
+    public void stateEntered(final State<WorkflowState, WorkflowEvent> state) {
         log.debug("stateEntered state:{}", state);
+        if (state.getId().isMandatoryToSendToClient()) {
+            log.debug("send to client");
+        }
     }
 
     @Override
-    public void stateExited(State<WorkflowState, WorkflowEvent> state) {
+    public void stateExited(final State<WorkflowState, WorkflowEvent> state) {
         log.debug("stateExited state:{}", state);
     }
 
     @Override
-    public void transition(Transition<WorkflowState, WorkflowEvent> transition) {
+    public void transition(final Transition<WorkflowState, WorkflowEvent> transition) {
         log.debug("transition transition:{}", transition);
     }
 
     @Override
-    public void transitionStarted(Transition<WorkflowState, WorkflowEvent> transition) {
+    public void transitionStarted(final Transition<WorkflowState, WorkflowEvent> transition) {
         log.info("transitionStarted transition:{}", transition);
     }
 
     @Override
-    public void transitionEnded(Transition<WorkflowState, WorkflowEvent> transition) {
+    public void transitionEnded(final Transition<WorkflowState, WorkflowEvent> transition) {
         log.debug("transitionStarted transition:{}", transition);
     }
 
     @Override
-    public void stateMachineStarted(StateMachine<WorkflowState, WorkflowEvent> stateMachine) {
+    public void stateMachineStarted(final StateMachine<WorkflowState, WorkflowEvent> stateMachine) {
         log.debug("stateMachineStarted stateMachine:{}", stateMachine);
     }
 
     @Override
-    public void stateMachineStopped(StateMachine<WorkflowState, WorkflowEvent> stateMachine) {
+    public void stateMachineStopped(final StateMachine<WorkflowState, WorkflowEvent> stateMachine) {
         log.debug("stateMachineStopped stateMachine:{}", stateMachine);
     }
 
     @Override
-    public void eventNotAccepted(Message<WorkflowEvent> event) {
+    public void eventNotAccepted(final Message<WorkflowEvent> event) {
         log.debug("eventNotAccepted event:{}", event);
     }
 
     @Override
-    public void extendedStateChanged(Object key, Object value) {
+    public void extendedStateChanged(final Object key, final Object value) {
         log.debug("extendedStateChanged key:{}, value={}", key, value);
     }
 
     @Override
-    public void stateMachineError(StateMachine<WorkflowState, WorkflowEvent> stateMachine, Exception exception) {
+    public void stateMachineError(final StateMachine<WorkflowState, WorkflowEvent> stateMachine, final Exception exception) {
         log.debug("stateMachineError stateMachine:{}, exception:{}", stateMachine, exception);
     }
 
     @Override
-    public void stateContext(StateContext<WorkflowState, WorkflowEvent> stateContext) {
+    public void stateContext(final StateContext<WorkflowState, WorkflowEvent> stateContext) {
         log.debug("stateContext stateContext:{}", stateContext);
     }
 }
