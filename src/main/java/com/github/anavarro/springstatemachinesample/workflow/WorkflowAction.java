d3baa771ecea451433ddb92d8491636bd53621f9
Alexandre Navarro <alexandre.j.navarro@gmail.com>
2016-07-26 21:01:08 +0200

    Update WorkflowAction.java

--- src/main/java/com/github/anavarro/springstatemachinesample/workflow/WorkflowAction.java
+++ src/main/java/com/github/anavarro/springstatemachinesample/workflow/WorkflowAction.java
@@ -6,79 +6,72 @@ import org.springframework.statemachine.StateContext;
 import static com.github.anavarro.springstatemachinesample.workflow.WorkflowEvent.*;
 
 /**
- * Created by anavarro on 24/07/16.
+ * Created by anavarro on 26/07/16.
  */
 @Slf4j
 public final class WorkflowAction {
 
-    public void storeRfq(final StateContext<WorkflowState, WorkflowEvent> stateContext) {
-        log.info("storeRfq is starting ...");
-        log.info("storeRfq finished.");
-        stateContext.getStateMachine().sendEvent(STORING_PRICING_INFO);
+    public void saveRfq(final StateContext<WorkflowState, WorkflowEvent> stateContext) {
+        log.info("saveRfq is starting ... state:{}", stateContext.getStateMachine().getState().getId());
+        log.info("saveRfq finished.");
+        stateContext.getStateMachine().sendEvent(SAVING_PRICING_INFO_EVENT);
     }
 
-    public void storePricingInfo(final StateContext<WorkflowState, WorkflowEvent> stateContext) {
-        log.info("storePricingInfo is starting ...");
-        log.info("storePricingInfo finished.");
-        stateContext.getStateMachine().sendEvent(SAVING_STATUS_NEW);
+    public void savePricingInfo(final StateContext<WorkflowState, WorkflowEvent> stateContext) {
+        log.info("savePricingInfo is starting ... state:{}", stateContext.getStateMachine().getState().getId());
+        log.info("savePricingInfo finished.");
+        stateContext.getStateMachine().sendEvent(ASKING_IS_BETA_USER_EVENT);
     }
 
-    public void saveStatusNew(final StateContext<WorkflowState, WorkflowEvent> stateContext) {
-        log.info("saveStatusNew is starting ...");
-        log.info("saveStatusNew finished.");
-        stateContext.getStateMachine().sendEvent(REQUESTING_BETA_USER_INFO);
+    public boolean askIsABetaUser(final StateContext<WorkflowState, WorkflowEvent> stateContext) {
+        log.info("askIsABetaUser is starting ... state:{}", stateContext.getStateMachine().getState().getId());
+        log.info("askIsABetaUser finished.");
+        final boolean isBetaUser = true;
+        return isBetaUser;
     }
 
-    public boolean requestBetaUserInfo(final StateContext<WorkflowState, WorkflowEvent> stateContext) {
-        log.info("requestBetaUserInfo is starting ...");
-        log.info("requestBetaUserInfo finished.");
-        return true;
+    public void askPrice(final StateContext<WorkflowState, WorkflowEvent> stateContext) {
+         log.info("askPrice is starting ... state:{}", stateContext.getStateMachine().getState().getId());
+         log.info("askPrice finished.");
     }
 
-    public boolean sendRfqToDefaultPricer(final StateContext<WorkflowState, WorkflowEvent> stateContext) {
-        log.info("sendRfqToDefaultPricer is starting ...");
-        log.info("sendRfqToDefaultPricer finished.");
-        return true;
-    }
-
-    public boolean receivePrice(final StateContext<WorkflowState, WorkflowEvent> stateContext) {
-        log.info("receivePrice is starting ...");
-        log.info("receivePrice finished.");
+    public boolean receiveAckPrice(final StateContext<WorkflowState, WorkflowEvent> stateContext) {
+        log.info("receiveAckPrice is starting ... state:{}", stateContext.getStateMachine().getState().getId());
+        log.info("receiveAckPrice finished.");
         return true;
     }
 
     public void cancelRfq(final StateContext<WorkflowState, WorkflowEvent> stateContext) {
-        log.info("cancelRfq is starting ...");
+        log.info("cancelRfq is starting ... state:{}", stateContext.getStateMachine().getState().getId());
         log.info("cancelRfq finished.");
     }
 
     public void savePricingOtherInfo(final StateContext<WorkflowState, WorkflowEvent> stateContext) {
-        log.info("savePricingOtherInfo is starting ...");
+        log.info("savePricingOtherInfo is starting ... state:{}", stateContext.getStateMachine().getState().getId());
         log.info("savePricingOtherInfo finished.");
-        stateContext.getStateMachine().sendEvent(APPLYING_MARGIN);
+        stateContext.getStateMachine().sendEvent(APPLYING_MARGIN_EVENT);
     }
 
     public void applyMargin(final StateContext<WorkflowState, WorkflowEvent> stateContext) {
-        log.info("applyMargin is starting ...");
+        log.info("applyMargin is starting ... state:{}", stateContext.getStateMachine().getState().getId());
         log.info("applyMargin finished.");
-        stateContext.getStateMachine().sendEvent(DEFAULTING_BOOKING_INFO);
+        stateContext.getStateMachine().sendEvent(DEFAULTING_BOOKING_INFO_EVENT);
     }
 
     public void defaultBookingInfo(final StateContext<WorkflowState, WorkflowEvent> stateContext) {
-        log.info("defaultBookingInfo is starting ...");
+        log.info("defaultBookingInfo is starting ... state:{}", stateContext.getStateMachine().getState().getId());
         log.info("defaultBookingInfo finished.");
-        stateContext.getStateMachine().sendEvent(REQUESTING_PDC_CALCULATION);
+        stateContext.getStateMachine().sendEvent(ASKING_PDC_COMPUTATION_EVENT);
     }
 
-    public void requestPdcCalculation(final StateContext<WorkflowState, WorkflowEvent> stateContext) {
-        log.info("requestPdcCalculation is starting ...");
-        log.info("requestPdcCalculation finished.");
-        stateContext.getStateMachine().sendEvent(REQUESTING_PDC_CALCULATION);
+    public void requestPdcComputation(final StateContext<WorkflowState, WorkflowEvent> stateContext) {
+        log.info("requestPdcComputation is starting ... state:{}", stateContext.getStateMachine().getState().getId());
+        log.info("requestPdcComputation finished.");
     }
 
-    public void sendTransactionToOnyxRequest(final StateContext<WorkflowState, WorkflowEvent> stateContext) {
-        log.info("sendTransactionToOnyxRequest is starting ...");
-        log.info("sendTransactionToOnyxRequest finished.");
+    public void sendTransaction(final StateContext<WorkflowState, WorkflowEvent> stateContext) {
+        log.info("sendTransaction is starting ... state:{}", stateContext.getStateMachine().getState().getId());
+        log.info("sendTransaction finished.");
     }
 
 }
