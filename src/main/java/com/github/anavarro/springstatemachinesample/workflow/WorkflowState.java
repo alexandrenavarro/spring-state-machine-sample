f57f4f7cc672b7a4174fbe4aefdd94629f7d15c2
Alexandre Navarro <alexandre.j.navarro@gmail.com>
2016-07-26 21:02:16 +0200

    Update WorkflowState.java

--- src/main/java/com/github/anavarro/springstatemachinesample/workflow/WorkflowState.java
+++ src/main/java/com/github/anavarro/springstatemachinesample/workflow/WorkflowState.java
@@ -1,24 +1,34 @@
 package com.github.anavarro.springstatemachinesample.workflow;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 /**
- * Created by anavarro on 23/07/16.
+ * Created by anavarro on 26/07/16.
  */
+@AllArgsConstructor
 public enum WorkflowState {
-    INITIATED,
-    RFQ_STORAGE_SUCCEEDED,
-    PRICING_INFO_STORAGE_SUCCEEDED,
-    LIFE_SAVING_SUCCEEDED,
-    BETA_USER_CONFIRMED,
-    BETA_USER_DENIED,
-    OTC_QUOTE_REQUEST_ACCEPTED,
-    QUOTE_REQUEST_CANCELLED,
-    PRICING_ACK_INFO_SAVING_SUCEEDED,
-    OTC_QUOTED,
-    PRICING_OTHER_INFO_SAVING_SUCCEEDED,
-    MARGIN_RESPONSE_RECEIVED,
-    DEFAULT_BOOKING_INFO_REQUEST_SUCCEEDED,
-    PDC_CALCULATION_REQUEST_SUCCEEDED,
-    PDC_RESULT_REQUEST_SUCCEEDED,
-    TRANSACTION_SENT_ACKNOWLEDGED
+
+    INITIATED_STATE(false, "PRE_NEW"),
+    RFQ_SAVED_STATE(false, "PRE_NEW"),
+    PRICING_INFO_SAVED_STATE(true, "NEW"),
+    IS_A_BETA_USER_STATE(false, "PRE_BEING_PRICE"),
+    IS_NOT_A_BETA_USER_STATE(false, "PRE_BEING_PRICE"),
+    PRICE_ASKED_STATE(true, "BEING_PRICE"),
+    PRICE_ASK_CANCELLED_STATE(false, "PRE_PRICED"),
+    PRICE_ASK_ACKED_STATE(false, "PRE_PRICED"),
+    PRICING_OTHER_INFO_SAVED_STATE(true, "PRICED"),
+    MARGIN_SAVED_STATE(false, "PRE_DEAL_VALIDATED"),
+    DEFAULT_BOOKING_INFO_SAVED_STATE(false, "PRE_DEAL_VALIDATED"),
+    PDC_COMPUTATION_ASKED_STATE(false, "PRE_DEAL_VALIDATED"),
+    PDC_COMPUTATION_SAVED_STATE(false, "PRE_DEAL_VALIDATED"),
+    TRANSACTION_SENT_STATE(true, "PRE_DEAL_VALIDATED");
+    // TODO add the other states
+
+    @Getter
+    private boolean mandatoryToSendToClient;
+    @Getter
+    private String functionalState;
+
 
 }
